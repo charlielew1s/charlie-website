8825f300c0f6232a5dd1aaf02a3549b86cd3c172
charlielew1s <91842295+charlielew1s@users.noreply.github.com>
2023-10-19 20:05:28 +0000

    latestupdate

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,44 +2,36 @@ import React, { useState } from 'react';
 import styles from './Home.module.css';
 
 function Home() {
-    const [posts, setPosts] = useState([]);
-    const [showPosts, setShowPosts] = useState(false); // state to control when to display the posts
 
-    const fetchPosts = async () => {
-        try {
-            const response = await fetch('https://us-central1-charlie-website-2550b.cloudfunctions.net/getPosts');
-            const data = await response.json();
-            setPosts(data);
-            setShowPosts(true); // Display posts after fetching
-        } catch (error) {
-            console.error("Error fetching posts:", error);
-        }
-    }
-
+    const [data, setData] = useState(null);
+   
     const logout = () => {
         localStorage.clear();
         window.location.reload();
     }
 
+    const fetchData = async () => {
+        try {
+          let response = await fetch('https://us-central1-charlie-website-2550b.cloudfunctions.net/getPosts');
+          let result = await response.json();
+          setData(result);
+        } catch (error) {
+          console.error("There was an error fetching the data:", error);
+        }
+      };
+
     return (
         <div>
             <button className={styles.button} onClick={logout}>Logout</button>
-            <button onClick={fetchPosts}>Fetch Posts</button>
-            
-            {showPosts && (
-                <div className={styles.postsContainer}>
-                    {posts.map(post => (
-                        <div key={post.id} className={styles.post}>
-                            {/* Render post content here. 
-                                 This assumes each post has an 'id' and you can adjust accordingly. */}
-                            <h2>{post.title}</h2>
-                            <p>{post.content}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
+            <button onClick={fetchData}>Fetch Data</button>
+            {data && (
+        <div>
+          <h3>Output:</h3>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Home;
